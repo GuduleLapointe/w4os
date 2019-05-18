fafe7c7b8953f5517df02314d8fae1682f8a83a1
Olivier van Helden (montgomery) <olivier@van-helden.net>
2019-05-18 19:00:02 -0400

    Added shortcode explanation

--- admin/status.php
+++ admin/status.php
@@ -1,8 +1,12 @@
 <div class="wrap">
 	<!-- <h1>OpenSimulator</h1>	 -->
 	<h1><?= esc_html(get_admin_page_title()); ?></h1>
-  <h2><?php echo __("Grid info") ?></h2>
-  <?php echo w4os_gridinfo_shortcode(); ?>
-  <h2><?php echo __("Grid status") ?></h2>
-  <?php echo w4os_gridstatus_shortcode(); ?>
+		<dt><code>[gridinfo]</code> General information (grid name and login uri)</dt>
+			<dd>
+		<?php echo w4os_gridinfo_shortcode(); ?>
+	</dd>
+	<p>
+	<dt>
+		<code>[gridstatus]</code> Online users, regions, etc.</dt>
+<dd>		<?php echo w4os_gridstatus_shortcode(); ?></dd>
 </div>
