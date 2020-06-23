f68db4fe0cec93b837df73c959349a187e70fa5c
Gudule Lapointe <gudule@speculoos.world>
2020-06-23 19:30:51 -0400

    v0.10.0.1 translations updates

--- w4os.php
+++ w4os.php
@@ -6,7 +6,7 @@
  *
  * Plugin Name:       W4OS OpenSimulator Interface
  * Description:       WordPress interface for OpenSimulator.
- * Version:           0.10
+ * Version:           0.10.0.1
  * Author:            Speculoos World
  * Author URI:        https://speculoos.world
  * Plugin URI:        https://git.magiiic.com/opensimulator/w4os
