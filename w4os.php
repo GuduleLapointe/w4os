06bd8284894bec69dcdfb60f192067def26ac6f1
Olivier van Helden <olivier@van-helden.net>
2021-02-07 14:19:50 -0400

    v1.0.6.6 * replaced banners png with jpg

--- w4os.php
+++ w4os.php
@@ -6,7 +6,7 @@
  *
  * Plugin Name:       W4OS OpenSimulator Interface
  * Description:       WordPress interface for OpenSimulator.
- * Version:           1.0.6.5
+ * Version:           1.0.6.6
  * Author:            Speculoos World
  * Author URI:        https://speculoos.world
  * Plugin URI:        https://git.magiiic.com/opensimulator/w4os
