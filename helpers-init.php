827d90d8c8ebb968b8d63e00755ddb7b83b9f7b5
Gudule Lapointe <gudule@speculoos.world>
2022-01-14 19:56:07 -0400

    added provide economy helpers options

---
+++ helpers-init.php
@@ -0,0 +1,12 @@
+<?php namespace OpenSimCurrency;
+if ( ! defined( 'W4OS_PLUGIN' ) ) die;
+
+if(get_option('w4os_provide_economy_helpers') &! empty(W4OS_GRID_INFO['economy']) ) {
+  $economy = parse_url(W4OS_GRID_INFO['economy'])['path'];
+  $url = getenv('REDIRECT_URL');
+  if(preg_match(":^$economy(currency.php|landtool.php):", $url)) {
+    $helper = preg_replace(":^$economy:", "", $url);
+    require($helper);
+    die();
+  }
+}
