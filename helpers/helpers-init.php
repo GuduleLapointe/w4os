ece1d6f6a4b636d4dce6d854482da7dc842889db
Gudule Lapointe <gudule@speculoos.world>
2022-01-21 02:53:45 -0400

    avoid warnings about numerical values

--- helpers/helpers-init.php
+++ helpers/helpers-init.php
@@ -4,7 +4,7 @@ if ( ! defined( 'W4OS_PLUGIN' ) ) die;
 
 $url = getenv('REDIRECT_URL');
 
-if(get_option('w4os_provide_economy_helpers') &! empty(W4OS_GRID_INFO['economy']) ) {
+if(get_option('w4os_provide_economy_helpers') == true &! empty(W4OS_GRID_INFO['economy']) ) {
   $economy = parse_url(W4OS_GRID_INFO['economy'])['path'];
   if(preg_match(":^$economy(currency.php|landtool.php):", $url)) {
     $helper = preg_replace(":^$economy:", "", $url);
@@ -13,14 +13,16 @@ if(get_option('w4os_provide_economy_helpers') &! empty(W4OS_GRID_INFO['economy']
   }
 }
 
-if ( get_option('w4os_provide_offline_messages') &! empty(W4OS_GRID_INFO['message']) ) {
+if ( get_option('w4os_provide_offline_messages') == true &! empty(W4OS_GRID_INFO['message']) ) {
   $message = parse_url(W4OS_GRID_INFO['message'])['path'];
   if(preg_match(":^$message/(SaveMessage|RetrieveMessages|offlineim)/:", "$url/")) {
     require('offline.php');
+  } else if($message == $url) {
+    die(); // ignore but don't trigger an error
   }
 }
 
-if ( get_option('w4os_provide_search') ) {
+if ( get_option('w4os_provide_search') == true ) {
   if(! empty(get_option('w4os_search_url'))) {
     $search = parse_url(get_option('w4os_search_url'))['path'];
     if(preg_match(":^$search/:", "$url/")) {
