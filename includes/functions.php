add48e6bede8d6ee971121424b0c6db31a0bd0f7
Gudule Lapointe <gudule@speculoos.world>
2022-01-22 12:58:55 -0400

    fix w4os_genuuid(), now returns a valid v4 uuid

--- includes/functions.php
+++ includes/functions.php
@@ -54,41 +54,21 @@ function w4os_notice ($message, $class="", $id = '', $context = '') {
 }
 
 function w4os_gen_uuid() {
- $uuid = array(
-  'time_low'  => 0,
-  'time_mid'  => 0,
-  'time_hi'  => 0,
-  'clock_seq_hi' => 0,
-  'clock_seq_low' => 0,
-  'node'   => array()
- );
-
- $uuid['time_low'] = mt_rand(0, 0xffff) + (mt_rand(0, 0xffff) << 16);
- $uuid['time_mid'] = mt_rand(0, 0xffff);
- $uuid['time_hi'] = (4 << 12) | (mt_rand(0, 0x1000));
- $uuid['clock_seq_hi'] = (1 << 7) | (mt_rand(0, 128));
- $uuid['clock_seq_low'] = mt_rand(0, 255);
-
- for ($i = 0; $i < 6; $i++) {
-  $uuid['node'][$i] = mt_rand(0, 255);
- }
-
- $uuid = sprintf('%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x',
-  $uuid['time_low'],
-  $uuid['time_mid'],
-  $uuid['time_hi'],
-  $uuid['clock_seq_hi'],
-  $uuid['clock_seq_low'],
-  $uuid['node'][0],
-  $uuid['node'][1],
-  $uuid['node'][2],
-  $uuid['node'][3],
-  $uuid['node'][4],
-  $uuid['node'][5]
- );
-
- return $uuid;
-}
+	return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+  // 32 bits for "time_low"
+  mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+  // 16 bits for "time_mid"
+  mt_rand(0, 0xffff),
+  // 16 bits for "time_hi_and_version",
+  // four most significant bits holds version number 4
+  mt_rand(0, 0x0fff) | 0x4000,
+  // 16 bits, 8 bits for "clk_seq_hi_res",
+  // 8 bits for "clk_seq_low",
+  // two most significant bits holds zero and one for variant DCE1.1
+  mt_rand(0, 0x3fff) | 0x8000,
+  // 48 bits for "node"
+  mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+);}
 
 function w4os_admin_notice($notice, $class='info', $dismissible=true ) {
   if(empty($notice)) return;
