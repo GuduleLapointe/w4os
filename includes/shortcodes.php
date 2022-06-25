GuduleLapointe/w4os

commit 1b1eae765b174f1ffcd0778e87cec8553ee6511a

* fix fatal errors in helpers when database is not connected

includes/shortcodes.php
@@ -123,6 +123,8 @@ function w4os_gridinfo_html($atts = [], $args=[])
 	extract( $args );
 
 	isset($atts['title']) ? $title=$atts['title'] : $title=__("Grid info", 'w4os');
+	$before_title = (isset($before_title)) ? $before_title : '';
+	$after_title = (isset($after_title)) ? $before_title : '';
 	$content = $before_title . $title . $after_title;
 	$info=array(
 		__("Grid name", 'w4os') => esc_attr(get_option('w4os_grid_name')),
@@ -150,7 +152,8 @@ function w4os_gridstatus_html($atts = [], $args = [])
 	$filter="";
 
 	isset($atts['title']) ? $title=$atts['title'] : $title=__("Grid status", 'w4os');
-
+	$before_title = (isset($before_title)) ? $before_title : '';
+	$after_title = (isset($after_title)) ? $before_title : '';
 	$content = $before_title . $title . $after_title;
 
 	$status=w4os_grid_status_text();

helpers/eventsparser.php
@@ -106,8 +106,10 @@ foreach($json as $json_event) {
   $events[] = $fields;
 }
 
-$SearchDB->query("DELETE FROM events");
-foreach($events as $event) {
-  $result = $SearchDB->insert('events', $event);
-  if(!$result) error_log("error while inserting new events)");
+if(is_object($SearchDB) && $SearchDB->connected) {
+  $SearchDB->query("DELETE FROM events");
+  foreach($events as $event) {
+    $result = $SearchDB->insert('events', $event);
+    if(!$result) error_log("error while inserting new events)");
+  }
 }

helpers/includes/databases.php
@@ -6,14 +6,15 @@
 class OSPDO extends PDO {
 	public function __construct($dsn, $username=null, $password=null, $driver_options=null)
 	{
-		@parent::__construct($dsn, $username, $password, $driver_options);
 		try {
+			@parent::__construct($dsn, $username, $password, $driver_options);
 			$this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$this->connected = true;
 		}
 		catch(PDOException $e)
 		{
-		  error_log($e);
+			error_log("Could not connect to database $dsn as $username");
+		  // error_log($e);
 			$this->connected = false;
 		}
 	}
@@ -51,6 +52,9 @@ class OSPDO extends PDO {
 
 function tableExists($pdo, $tables) {
 	if(!is_object($pdo)) return false;
+	if(!$pdo->connected) return false;
+	// error_log("pdo " . print_r($pdo, true));
+
   if(is_string($tables)) $tables=array($tables);
   foreach($tables as $table) {
     // Try a select statement against the table

helpers/includes/search.php
@@ -16,6 +16,8 @@
  */
 
 function ossearch_db_tables($db) {
+  if(!$db->connected) return false;
+
   $query = $db->prepare("CREATE TABLE IF NOT EXISTS `allparcels` (
     `regionUUID` char(36) NOT NULL,
     `parcelname` varchar(255) NOT NULL,
@@ -225,7 +227,7 @@ catch(PDOException $e) {
   $SearchDB = NULL;
 }
 
-if($SearchDB) {
+if($SearchDB && $SearchDB->connected) {
   // <tl;tr> To avoid data loss, fatal errors or conflicts, we use regionsregister
   // table instead of regions if it seems to be a robust database. For obscure and
   // historical reasons, search regions table has the same name as robust regions

helpers/parser.php
@@ -302,24 +302,26 @@ function hostScan($hostname, $port, $xmlcontent)
   }
 }
 
-// $sql = "SELECT host, port FROM hostsregister WHERE nextcheck<$now AND checked=0 AND failcounter<10 LIMIT 0,100";
-$sql = "SELECT host, port FROM hostsregister WHERE nextcheck<$now AND checked=0 LIMIT 0,100";
-// $sql = "SELECT host, port FROM hostsregister WHERE checked=0 LIMIT 0,100";
-$jobsearch = $SearchDB->query($sql);
-
-//
-// If the sql query returns no rows, all entries in the hostsregister
-// table have been checked. Reset the checked flag and re-run the
-// query to select the next set of hosts to be checked.
-//
-
-if ($jobsearch->rowCount() == 0)
-{
-  $jobsearch = $SearchDB->query("UPDATE hostsregister SET checked = 0");
+if($SearchDB && $SearchDB->connected) {
+  // $sql = "SELECT host, port FROM hostsregister WHERE nextcheck<$now AND checked=0 AND failcounter<10 LIMIT 0,100";
+  $sql = "SELECT host, port FROM hostsregister WHERE nextcheck<$now AND checked=0 LIMIT 0,100";
+  // $sql = "SELECT host, port FROM hostsregister WHERE checked=0 LIMIT 0,100";
   $jobsearch = $SearchDB->query($sql);
-}
 
-while ($jobs = $jobsearch->fetch(PDO::FETCH_NUM))
-hostCheck($jobs[0], $jobs[1]);
+  //
+  // If the sql query returns no rows, all entries in the hostsregister
+  // table have been checked. Reset the checked flag and re-run the
+  // query to select the next set of hosts to be checked.
+  //
+
+  if ($jobsearch->rowCount() == 0)
+  {
+    $jobsearch = $SearchDB->query("UPDATE hostsregister SET checked = 0");
+    $jobsearch = $SearchDB->query($sql);
+  }
+
+  while ($jobs = $jobsearch->fetch(PDO::FETCH_NUM))
+  hostCheck($jobs[0], $jobs[1]);
+}
 
 die();

includes/profile-page.php
@@ -207,7 +207,7 @@ add_action( 'template_include', function( $template ) {
     exit();
   }
 
-  $user_agent = $_SERVER['HTTP_USER_AGENT'];
+  $user_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
   if(strpos( $user_agent, ' - SecondLife/') !== false) {
     return plugin_dir_path(__DIR__) . "templates/page-profile-viewer.php"; die();
   }

commit 752b1d480cb1bfe208d9c58ba6b834da0234b0ad

fix styling in grid info and grid status shortcode output (bug introduced in 1b1eae765b174f1ffcd0778e87cec8553ee6511a)

includes/shortcodes.php
@@ -124,7 +124,7 @@ function w4os_gridinfo_html($atts = [], $args=[])
 
 	isset($atts['title']) ? $title=$atts['title'] : $title=__("Grid info", 'w4os');
 	$before_title = (isset($before_title)) ? $before_title : '';
-	$after_title = (isset($after_title)) ? $before_title : '';
+	$after_title = (isset($after_title)) ? $after_title : '';
 	$content = $before_title . $title . $after_title;
 	$info=array(
 		__("Grid name", 'w4os') => esc_attr(get_option('w4os_grid_name')),