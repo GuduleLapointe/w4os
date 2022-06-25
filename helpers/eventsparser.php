4b8e7f5d5dde5abe77aa51696506ba17cfbd557a
Gudule Lapointe <gudule@speculoos.world>
2022-06-25 13:58:52 -0400

    * fix fatal error in helpers with poorly encoded unicode text

--- helpers/eventsparser.php
+++ helpers/eventsparser.php
@@ -82,12 +82,15 @@ foreach($json as $json_event) {
 
   $slurl = opensim_format_tp($json_event['hgurl'], TPLINK_TXT );
   $links = opensim_format_tp($json_event['hgurl'], TPLINK_APPTP + TPLINK_HOP);
-  $description = strip_tags(html_entity_decode($json_event['description']));
+  $description = strip_tags(html_entity_decode(utf8_encode(utf8_decode($json_event['description']))));
   $description = "$links\n\n$description";
+  // $title = utf8_encode(utf8_decode($json_event['title']));
+  $title = strip_tags(html_entity_decode(utf8_encode(utf8_decode($json_event['title']))));
+  error_log($title);
 
   $fields = array(
     'owneruuid' => EVENTS_NULL_KEY, // Not implemented
-    'name' => $json_event['title'],
+    'name' => $title,
     // 'eventid' => $json_event['eventid'],
     'creatoruuid' => EVENTS_NULL_KEY, // Not implemented
     'category' => getEventCategory($json_event['categories']),
