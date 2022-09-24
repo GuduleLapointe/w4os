d24f7d47c58dec20d77fbd5f411d294e1788ba9c
Gudule Lapointe <gudule@speculoos.world>
2022-09-24 13:55:05 -0400

    fix warning Undefined variable $pagenow

--- admin/admin-init.php
+++ admin/admin-init.php
@@ -246,7 +246,7 @@ add_action('restrict_manage_users', 'w4os_users_filter_avatars');
  * Now we can launch the actual admin sections
  */
 require_once __DIR__ . '/settings.php';
-if($pagenow == "index.php") require_once __DIR__ .'/dashboard.php';
+if((isset($pagenow)) && $pagenow == "index.php") require_once __DIR__ .'/dashboard.php';
 
 /**
  * Add post state to special pages in pages list
