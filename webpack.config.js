01436458615773e7eb61ba58d5d922fcf9eba3d5
Gudule Lapointe <gudule@speculoos.world>
2023-03-17 18:46:51 -0400

    added npm libraries

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
+// Configuration object.
+const config = {
+  ...defaultConfig,
+	entry: {
+		// 'admin/admin': './dev/admin/admin-index.js',
+    // '../blocks/popular-places/index': './src/blocks/popular-places/index.js',
+    '../blocks/events/index': './src/blocks/events/index.js',
+	},
+	output: {
+    filename: '[name].js',
+    // Specify the path to the JS files.
+    path: path.resolve( __dirname, 'build' ),
+	},
+}
+
+// Export the config object.
+module.exports = config;
