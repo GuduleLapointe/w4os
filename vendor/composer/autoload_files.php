144e51ebd2a0ea687a9548d1914df17ae1361c31
Gudule Lapointe <gudule@speculoos.world>
2023-07-08 10:35:56 -0400

    added meta-box conditional logic

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -10,5 +10,6 @@ return array(
     '687e88d103d3c7b7e29ca5e836cb2272' => $vendorDir . '/meta-box/mb-settings-page/mb-settings-page.php',
     'be497f88b773aa5c9974da46821effab' => $vendorDir . '/meta-box/mb-admin-columns/mb-admin-columns.php',
     'd46e9eb4d08a138d32890bfd33c08c32' => $vendorDir . '/meta-box/meta-box-columns/meta-box-columns.php',
+    'd625401fcd6c5ef99e9ddf76eb29ba78' => $vendorDir . '/meta-box/meta-box-conditional-logic/meta-box-conditional-logic.php',
     '3cdaf8a7feac7dab25a504ea4957b65a' => $vendorDir . '/woocommerce/action-scheduler/action-scheduler.php',
 );
