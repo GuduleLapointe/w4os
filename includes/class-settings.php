830fd83ff5b254263f6637ba5c6b4c53dbd844a2
Gudule Lapointe <gudule@speculoos.world>
2023-07-10 00:44:01 -0400

    Addempt to add a custom db field type, unfinished

--- includes/class-settings.php
+++ includes/class-settings.php
@@ -44,6 +44,17 @@ class W4OS_Settings extends W4OS_Loader {
 				'callback' => 'register_settings_fields',
 				// 'priority' => 5,
 			),
+			array(
+				'hook' => 'rwmb_w4osdb_field_type_html',
+				'callback' => 'db_field_html',
+				'accepted_args' => 3,
+			),
+			array(
+				'hook' => 'rwmb_w4osdb_field_type_value',
+				'callback' => 'db_field_value',
+				'accepted_args' => 4,
+			),
+
 			// array(
 			// 	'hook' => 'query_vars',
 			// 	'callback' => 'register_query_vars',
@@ -124,6 +135,46 @@ class W4OS_Settings extends W4OS_Loader {
 			],
 		];
 
+		$meta_boxes[] = [
+			'id'             => 'robust-db',
+			'settings_pages' => ['w4os_settings'],
+			'visible'        => [
+				'when'     => [['w4os_login_uri', '!=', '']],
+				'relation' => 'or',
+			],
+			'fields'         => [
+				[
+					'name'   => __( 'Database', 'w4os' ),
+					'id'     => $prefix . 'db',
+					'type'   => 'group',
+					'class'  => 'inline',
+					'save_field' => false,
+					'fields' => $this->db_fields(array(
+						'is_main' => true,
+						'host' => get_option( 'w4os_db_host' ),
+						'database' => get_option( 'w4os_db_database' ),
+						'user' => get_option( 'w4os_db_user' ),
+						'pass' => get_option( 'w4os_db_pass' ),
+					)),
+				],
+				array(
+	        'name' => 'Custom Field',
+	        'id' => $prefix . 'customdb',
+	        'type' => 'w4osdb_field_type',
+					'save_field' => false,
+					'std' => array(
+						'is_main' => true,
+						'type' => get_option( 'w4os_db_type', 'mysql' ),
+						'port' => get_option( 'w4os_db_port', 3306 ),
+						'host' => get_option( 'w4os_db_host' ),
+						'database' => get_option( 'w4os_db_database' ),
+						'user' => get_option( 'w4os_db_user' ),
+						'pass' => get_option( 'w4os_db_pass' ),
+					),
+	      ),
+			],
+		];
+
 		return $meta_boxes;
 	}
 
@@ -138,10 +189,147 @@ class W4OS_Settings extends W4OS_Loader {
 				}
 				update_option('w4os_login_uri', $login_uri);
 			}
-			return;
+		}
+
+		if( isset($_POST['nonce_robust-db']) && wp_verify_nonce( $_POST['nonce_robust-db'], 'rwmb-save-robust-db' ) ) {
+			// error_log(print_r($_POST, true));
 		}
 	}
 
+	private function db_fields( $values = [] ) {
+		$use_robust = isset($values['use_robust']) ? $values['use_robust'] : true;
+		$is_main = isset($values['is_main']) ? $values['is_main'] : false;
+		if($is_main) {
+			$visible_condition = true;
+			$use_robust = false;
+		} else {
+			$visible_condition = [
+				'when'     => [['use_robust', '!=', 1]],
+				'relation' => 'or',
+			];
+		}
+		return [
+			[
+					'name'  => __( 'Use ROBUST', 'w4os' ),
+					'id'    => 'use_robust',
+					'type'  => 'switch',
+					// 'disabled' => $is_main,
+					'hidden' => $is_main,
+					'style' => 'rounded',
+					'std' => $use_robust,
+			],
+			[
+				'name'    => __( 'Type', 'w4os' ),
+				'id'      => 'type',
+				'type'    => 'select',
+				'options' => [
+					'mysql' => __( 'MySQL', 'w4os' ),
+				],
+				'std' => empty($values['type']) ? 'mysql' : $values['type'],
+				'visible' => $visible_condition,
+			],
+			[
+				'name' => __( 'Hostname', 'w4os' ),
+				'id'   => 'host',
+				'type' => 'text',
+				'std' => empty($values['host']) ? 'localhost' : $values['host'],
+				'visible' => $visible_condition,
+			],
+			[
+				'name' => __( 'Port', 'w4os' ),
+				'id'   => 'port',
+				'type' => 'number',
+				'step' => 'any',
+				'std' => empty($values['port']) ? 3306 : $values['port'],
+				'visible' => $visible_condition,
+			],
+			[
+				'name' => __( 'Database Name', 'w4os' ),
+				'id'   => 'database',
+				'type' => 'text',
+				'std' => empty($values['database']) ? 'opensim' : $values['database'],
+				'visible' => $visible_condition,
+			],
+			[
+				'name' => __( 'Username', 'w4os' ),
+				'id'   => 'user',
+				'type' => 'text',
+				'std' => empty($values['user']) ? 'opensim' : $values['user'],
+				'visible' => $visible_condition,
+			],
+			[
+				'name' => __( 'Password', 'w4os' ),
+				'id'   => 'pass',
+				'type' => 'password',
+				'std' => empty($values['pass']) ? null : $values['pass'],
+				'visible' => $visible_condition,
+			],
+		];
+	}
+
+	public function render_field($value, $field ) {
+		// switch ($field['type']) {
+		// }
+
+		$output = '<label for="%1$s">%2$s';
+		switch($field['type']) {
+			case 'switch':
+			$field['type'] = 'checkbox';
+			$checked = checked($value);
+
+			default:
+			$type = $field['type'];
+			$checked = null;
+		}
+		$output .= '<input type="%3$s" name="%1$s" id="%1$s" value="%4$s" %5$s />';
+		$output .= '</label>';
+
+		$field_id = (empty($field['parent_id'])) ? $field['id'] : $field['parent_id'] . '[' . $field['id'] . ']';
+		error_log(
+ 			"args " . print_r(array(
+				$field_id,
+				$field['name'],
+				$type,
+				$value,
+				$checked,
+			), true)
+			. "field_id $field_id = $value"
+		);
+		$output = sprintf(
+			$output,
+			$field_id,
+			$field['name'],
+			$type,
+			$value,
+			$checked,
+		);
+
+		return $output;
+	}
+
+	public function db_field_html( $html, $field = null, $values = [] ) {
+		// Render the HTML output for the w4os db field type
+		// Use $field and $meta to access field settings and saved values
+		// $fields = array('use_robust', 'type', 'host', 'port', 'database', 'user', 'pass');
+		$subfields = $this->db_fields($values);
+
+		$output = '';
+		foreach($subfields as $subfield) {
+			$subfield['parent_id'] = $field['id'];
+			$output .= $this->render_field($values[$subfield['id']], $subfield);
+		}
+		return $output;
+	}
+
+	public static function db_field_value( $new, $field, $old, $object_id ) {
+		error_log(__CLASS__ . '::' . __METHOD__ . ' ' . print_r($new, true));
+		// Save the custom field value
+		// $new: New field value
+		// $old: Old field value
+		// $post_id: Current post ID
+		// $field: Field settings
+		return $new;
+	}
 }
 
 $this->loaders[]=new W4OS_Settings();
--- src/admin/index.js
+++ src/admin/index.js
@@ -1,3 +1,4 @@
+import "./settings.scss";
 import "./settings.js";
 import "./admin.scss";
 import "./models.scss";
