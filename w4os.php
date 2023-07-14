b48c782714fb19ff54f11496fcc909a4ef6de8d8
Gudule Lapointe <gudule@speculoos.world>
2023-07-13 21:28:44 -0400

    v2.4.3 * added Popular Places Divi Builder module * added separate web assets server settings page * added separate Shortcode admin page * added [popular-places] to Shortcode page * renamed shortcodes for clarity and constistency:   - [grid-info] instead of [gridinfo]   - [grid-status] instead of [gridstatus]   - [avatar-profile] instead of [gridprofile]   - Old shortcodes are kept for backwards compatibility * added warning when attempting to edit profile page (or any page generated by w4os) with Divi Builder. * prettier and more efficient db credentials options in settings page (getting ready for optimized use in the future)

--- w4os.php
+++ w4os.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name:       w4os - OpenSimulator Web Interface (dev)
+ * Plugin Name:       w4os - OpenSimulator Web Interface
  * Description:       WordPress interface for OpenSimulator (w4os).
- * Version:           2.4.3-dev.1031
+ * Version:           2.4.3
  * Author:            Speculoos World
  * Author URI:        https://speculoos.world
  * Plugin URI:        https://w4os.org/
