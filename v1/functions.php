681bf3ed86ab9bc03da23931e34b144df63ecb65
Olivier van Helden (grd01) <olivier@van-helden.net>
2025-01-02 06:06:12 +0000

    * fix arrays passed as if they were strings in w4os_array2table

--- v1/functions.php
+++ v1/functions.php
@@ -8,6 +8,9 @@ function w4os_array2table( $array, $class = '', $level = 1 ) {
 	if ( $level == 1 ) {
 		$result = '';
 		foreach ( $array as $key => $value ) {
+			if ( is_array( $value )) {
+				$value = join( ', ', $value );
+			}
 			$result .= '<tr><td class=gridvar>' . __( $key, 'w4os' ) . "</td><td class=gridvalue>$value</td></tr>";
 		}
 		if ( ! empty( $result ) ) {
