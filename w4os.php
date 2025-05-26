5aea843c811468a3a4f01b0978eba2828778c247
Gudule Lapointe <gudule@speculoos.world>
2025-05-26 14:10:55 +0200

    v2.9.5-beta-7 * attempt to fix PHP fatal error (wp-load.php missing in release)

--- w4os.php
+++ w4os.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       w4os - OpenSimulator Web Interface (dev)
  * Description:       WordPress interface for OpenSimulator (w4os).
- * Version:           2.9.5-beta-6
+ * Version:           2.9.5-beta-7
  * Author:            Speculoos World
  * Author URI:        https://speculoos.world
  * Plugin URI:        https://w4os.org/
